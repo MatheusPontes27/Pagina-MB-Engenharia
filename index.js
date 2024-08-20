cb41f4d8e6b7ec2f4587a96eca75670a3cf9894f
MatheusPontes27 <135027539+MatheusPontes27@users.noreply.github.com>
2024-08-20 20:56:52 -0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,15 @@
+
+
+window.sr = ScrollReveal({reset: true});
+
+sr.reveal('.hero', { duration: 1000 });
+
+sr.reveal('.section-partners', { duration: 2000 });
+
+sr.reveal('.sobre-nos', { duration: 2000 });
+
+sr.reveal('.service', { duration: 2000 });
+
+sr.reveal('.testimonial-container', { duration: 2000 });
+
+
